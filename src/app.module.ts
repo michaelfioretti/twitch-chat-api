6f8663d2c0e9a7e77f46d7e20ee490cbf2137815
Michael Fioretti <michaelfioretti1@gmail.com>
2025-04-15 11:48:06 -0700

    Add config module and mongoose to API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BitsModule } from './bits/bits.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BitsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    BitsModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z9zf2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
