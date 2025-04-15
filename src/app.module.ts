421122ff3b9a02bfc8113dcdf0270a3ea8b221b1
Michael Fioretti <michaelfioretti1@gmail.com>
2025-04-15 14:04:49 -0700

    Create Message schema for Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
