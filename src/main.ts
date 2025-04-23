dcc8685546fa4bc78d74b1648f2f8136634d9c92
Michael Fioretti <michaelfioretti1@gmail.com>
2025-04-23 11:26:11 -0700

    Add helmet to API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
